64a24078091bbd42db653693495ea4340076b495
etoyz <2905226519@qq.com>
2021-05-12 20:03:26 +0800

    统一UI & service/consumeType/addConsumeType api

--- src/main/java/com/sh/cloud/web/ConsumeTypeController.java
+++ src/main/java/com/sh/cloud/web/ConsumeTypeController.java
@@ -1,10 +1,15 @@
 package com.sh.cloud.web;
 
+import com.sft.member.bean.ConsumeType;
 import com.sft.member.obtain.consume.ConsumeTypeService;
+import com.sh.cloud.utils.PlatUserUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 @RequestMapping("service/consumeType")
@@ -12,4 +17,15 @@ public class ConsumeTypeController {
     @Resource
     ConsumeTypeService consumeTypeService;
 
+//    @RequestMapping("getConsumeType")
+//    @ResponseBody
+//    public List<ConsumeType> getConsumeType() {
+//        return consumeTypeService.getConsumeTypeList();
+//    }
+
+    @RequestMapping("addConsumeType")
+    @ResponseBody
+    public String addConsumeType(@RequestBody ConsumeType consumeType) {
+        return consumeTypeService.addConsumeType(PlatUserUtils.getCurrentLoginPlatUser(),consumeType);
+    }
 }
