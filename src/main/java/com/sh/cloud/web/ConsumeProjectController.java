7a3f1d59b22f9544740b1c4db8742c869b5a21ef
etoyz <2905226519@qq.com>
2021-05-13 20:36:49 +0800

    消费项目v1.0

--- src/main/java/com/sh/cloud/web/ConsumeProjectController.java
+++ src/main/java/com/sh/cloud/web/ConsumeProjectController.java
@@ -1,12 +1,15 @@
 package com.sh.cloud.web;
 
+import com.sft.member.bean.ConsumeProject;
 import com.sft.member.obtain.consume.ConsumeProjectService;
+import com.sh.cloud.utils.PlatUserUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("service/consumeProject")
@@ -14,9 +17,35 @@ public class ConsumeProjectController {
     @Resource
     ConsumeProjectService consumeProjectService;
 
-    @PostMapping("getProjectList")
+    @RequestMapping("getConsumeProjectList")
     @ResponseBody
-    public String getProjectList() {
-        return "..";
+    public Map<String, Object> getProjectList(@RequestParam String query, @RequestParam int page, @RequestParam int limit) {
+        Map<String, Object> ret = new HashMap<>();
+        ret.put("code", 0);
+        ret.put("msg", "");
+
+        ConsumeProject consumeProject = new ConsumeProject();
+        consumeProject.name = query;
+        List<ConsumeProject> data = consumeProjectService.getConsumeProjectList(consumeProject, page, limit);
+        ret.put("data", data);
+        ret.put("count", consumeProjectService.getConsumeProjectListCount(consumeProject));
+        return ret;
+    }
+
+    @PostMapping("addConsumeProject")
+    @ResponseBody
+    public String addConsumeProject(@RequestBody ConsumeProject consumeProject) {
+        return consumeProjectService.addConsumeProject(PlatUserUtils.getCurrentLoginPlatUser(), consumeProject);
+    }
+
+    @RequestMapping("deleteConsumeProject")
+    @ResponseBody
+    public String deleteConsumeProject(@RequestParam String id) {
+        ConsumeProject consumeProject = new ConsumeProject();
+        consumeProject.consumeProjectId = Integer.parseInt(id);
+        if (consumeProjectService.deleteConsumeProject(PlatUserUtils.getCurrentLoginPlatUser(), consumeProject))
+            return "删除成功！";
+        else
+            return "删除失败！";
     }
 }
