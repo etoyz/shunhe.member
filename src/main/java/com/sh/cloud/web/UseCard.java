1a5b25ccdc9c9b7a44053396760891f8432e687f
etoyz <2905226519@qq.com>
2021-06-15 13:31:21 +0800

    添加明细&提交审核beta0.1

--- src/main/java/com/sh/cloud/web/UseCard.java
+++ src/main/java/com/sh/cloud/web/UseCard.java
@@ -1,10 +1,16 @@
 package com.sh.cloud.web;
 
+import com.sft.member.bean.CouponCheck;
+import com.sft.member.bean.UserCoupon;
 import com.sft.member.obtain.pay.PayService;
+import com.sh.cloud.utils.PlatUserUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("service/useCard")
@@ -12,8 +18,16 @@ public class UseCard {
     @Resource
     PayService payService;
 
-//    @RequestMapping("getEnablePayCoupon")
-//    public String getEnablePayCoupon(){
-//        payService.getEnablePayCoupon();
-//    }
+    @RequestMapping("getEnablePayCoupon")
+    @ResponseBody
+    public List<UserCoupon> getEnablePayCoupon(String userid, String consumeProjectId) {
+        return payService.getEnablePayCoupon(userid, consumeProjectId);
+    }
+
+    @RequestMapping("submitForReview")
+    @ResponseBody
+    public String submitForReview(@RequestBody List<CouponCheck> list) {
+        payService.addUnCheckRecord(PlatUserUtils.getCurrentLoginPlatUser(), list);
+        return "成功！";
+    }
 }
