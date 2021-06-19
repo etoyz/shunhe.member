e0efac5df9eb5e04401452c6ae83bfd09e55d360
fqh <1660206164@qq.com>
2021-06-19 17:28:12 +0800

    The first step of couponStatistical.

---
+++ src/main/java/com/sh/cloud/web/CouponStatisticalController.java
@@ -0,0 +1,10 @@
+package com.sh.cloud.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("service/couponStatistical")
+public class CouponStatisticalController {
+
+}
